0151f9dba3c73e202399e5367cbd9ca8af3b1491
ruzal <ruzalkunwar@gmail.com>
2023-06-25 17:33:43 +0545

    feedback

--- public/admin/dashboard.php
+++ public/admin/dashboard.php
@@ -11,7 +11,20 @@
 
 <body>
 	<div class="flex">
-		<?php include('dashlayout.php') ?>
+		<?php include('dashlayout.php');
+
+		$qry1 = "SELECT * FROM collections WHERE collection_status = 'Completed'";
+		$res1 = mysqli_query($con,$qry1);
+		$row1 = mysqli_num_rows($res1);
+
+		$qry2 = "SELECT * FROM complaints WHERE complain_status = 'Rejected'";
+		$res2 = mysqli_query($con,$qry2);
+		$row2 = mysqli_num_rows($res2);
+
+		$qry3 = "SELECT * FROM complaints WHERE complain_status = 'new'";
+		$res3 = mysqli_query($con,$qry3);
+		$row3 = mysqli_num_rows($res3);
+		?>
 		<div class="h-full w-full p-5 ml-14 md:ml-64">
 			<div class="w-full p-5">
 				<h2 class="text-3xl border-b-2 border-blue-600">Dashboard</h2>
@@ -20,35 +33,33 @@
 				<!-- Card for pending pickups -->
 				<div class="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
 					<div class="bg-white rounded-lg shadow-md p-6 hover:bg-red-100">
-						<h2 class="text-lg font-semibold mb-4">Pending Pickups</h2>
+						<h2 class="text-lg font-semibold mb-4">Complaints</h2>
 						<div class="flex justify-between">
 							<div class="text-gray-500">Total</div>
-							<div class="text-2xl font-bold">10</div>
+							<div class="text-2xl font-bold"><?php echo $row3 ?></div>
 						</div>
 					</div>
 				</div>
 
-				<!-- Card for collected pickups -->
-				<div class="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
-					<div class="bg-white rounded-lg shadow-md p-6 hover:bg-green-100">
-						<h2 class="text-lg font-semibold mb-4">Collected Pickups</h2>
-						<div class="flex justify-between">
-							<div class="text-gray-500">Total</div>
-							<div class="text-2xl font-bold">50</div>
-						</div>
-					</div>
-				</div>
-
-				<!-- Card for cancelled pickups -->
 				<div class="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
 					<div class="bg-white rounded-lg shadow-md p-6 hover:bg-gray-200">
-						<h2 class="text-lg font-semibold mb-4">Cancelled Pickups</h2>
+						<h2 class="text-lg font-semibold mb-4">Cancelled Complaints</h2>
 						<div class="flex justify-between">
 							<div class="text-gray-500">Total</div>
-							<div class="text-2xl font-bold">5</div>
+							<div class="text-2xl font-bold"><?php echo $row2 ?> </div>
 						</div>
 					</div>
 				</div>
+				<div class="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+					<div class="bg-white rounded-lg shadow-md p-6 hover:bg-green-100">
+						<h2 class="text-lg font-semibold mb-4">Collected Bins</h2>
+						<div class="flex justify-between">
+							<div class="text-gray-500">Total</div>
+							<div class="text-2xl font-bold"><?php echo $row1 ?> </div>
+						</div>
+					</div>
+				</div>
+				
 				<?php
 				$qry = "SELECT * FROM garbagebins";
 				$result = mysqli_query($con, $qry);
@@ -57,7 +68,7 @@
 				<div class="w-full p-5">
 					<h2 class="text-3xl border-b-2 border-blue-600">Active Bins</h2>
 				</div>
-				<div class="container max-w-3xl px-4 mx-auto sm:px-8">
+				<div class="container max-w-full px-4 mx-auto sm:px-8">
 					<div class="py-8">
 						<div class="px-4 py-4 -mx-4 overflow-x-auto sm:-mx-8 sm:px-8">
 							<div class="inline-block min-w-full overflow-hidden rounded-lg shadow">
@@ -122,25 +133,6 @@
 										<?php } ?>
 									</tbody>
 								</table>
-								<div class="flex flex-col items-center px-5 py-5 bg-white xs:flex-row xs:justify-between">
-									<div class="flex items-center">
-										<button type="button" class="w-full p-4 text-base text-gray-600 bg-white border rounded-l-xl hover:bg-gray-100">
-											<svg width="9" fill="currentColor" height="8" class="" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-												<path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z">
-												</path>
-											</svg>
-										</button>
-										<button type="button" class="w-full px-4 py-2 text-base text-indigo-500 bg-white border-t border-b hover:bg-gray-100 ">
-											1
-										</button>
-										<button type="button" class="w-full p-4 text-base text-gray-600 bg-white border-t border-b border-r rounded-r-xl hover:bg-gray-100">
-											<svg width="9" fill="currentColor" height="8" class="" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-												<path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z">
-												</path>
-											</svg>
-										</button>
-									</div>
-								</div>
 							</div>
 						</div>
 					</div>
--- public/admin/feedback.php
+++ public/admin/feedback.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Feedback</title>
     <link rel="stylesheet" href="../style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
@@ -20,31 +21,124 @@
             <table class="min-w-full">
                 <thead>
                     <tr>
+                        <th class="px-6 py-3 bg-gray-100 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">#</th>
                         <th class="px-6 py-3 bg-gray-100 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Timestamp</th>
                         <th class="px-6 py-3 bg-gray-100 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
                         <th class="px-6 py-3 bg-gray-100 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
                         <th class="px-6 py-3 bg-gray-100 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Message</th>
+                        <th class="px-6 py-3 bg-gray-100 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php
-                    $sql = "SELECT * FROM feedback";
+                    $sql = "SELECT * FROM feedback WHERE status = 0";
                     $res = mysqli_query($con, $sql);
+                    $x = 1;
                     while ($data = mysqli_fetch_assoc($res)) {
-                        $timestamp = $data['timestamp']; 
+                        $timestamp = $data['timestamp'];
                         $date = date('F j, Y', strtotime($timestamp));
                     ?>
                         <tr>
+                            <td class="px-6 py-4 whitespace-nowrap"><?php echo $x ?></td>
                             <td class="px-6 py-4 whitespace-nowrap"> <?php echo $date ?></td>
                             <td class="px-6 py-4 whitespace-nowrap"><?php echo $data['name'] ?></td>
                             <td class="px-6 py-4 whitespace-nowrap"><?php echo $data['email'] ?></td>
                             <td class="px-6 py-4 whitespace-wrap"><?php echo $data['message'] ?></td>
+                            <td><button onclick="openPopupForm(<?php echo $data['id'] ?>,'<?php echo $data['name'] ?>','<?php echo $data['message'] ?>','<?php echo $data['email'] ?>');" class="px-3 py-2 bg-sky-400 rounded-lg text-white hover:bg-blue-700">Reply</button></td>
                         </tr>
-                    <?php } ?>
+                    <?php $x++;
+                    } ?>
                 </tbody>
             </table>
         </div>
     </div>
+
+    <div id="popupForm" class="hidden">
+        <div class="fixed inset-0 z-10 flex items-center justify-center backdrop-blur-md bg-gray-600 bg-opacity-40">
+            <div class=" w-2/5 bg-cyan-300 rounded-md shadow-lg">
+                <div class="px-10 py-5 w-full ">
+                    <div class="flex justify-between">
+                        <h2 class="text-xl font-semibold mb-4">Reply to User Feedback</h2>
+                        <span onclick=" closePopupForm()"> <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-toggle="default-modal">
+                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                </svg>
+                            </button>
+                        </span>
+                    </div>
+                    <div class="p-6">
+                        <form method="post">
+                            <input type="hidden" name="fid" id="fid" value="">
+                            <div class="mb-4">
+                                <label for="user-name" class="block font-medium text-gray-700 mb-2">User Name:</label>
+                                <input id="user-name" name="user-name" type="text" class="w-full border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:border-blue-500" readonly>
+                            </div>
+                            <div class="mb-4">
+                                <label for="user-email" class="block font-medium text-gray-700 mb-2">User Email:</label>
+                                <input id="user-email" name="user-email" type="text" class="w-full border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:border-blue-500" readonly>
+                            </div>
+                            <div class="mb-4">
+                                <label for="user-feedback" class="block font-medium text-gray-700 mb-2">User Feedback:</label>
+                                <textarea id="user-feedback" class="w-full border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:border-blue-500" rows="4" readonly></textarea>
+                            </div>
+                            <div class="mb-4">
+                                <label for="admin-reply" class="block font-medium text-gray-700 mb-2">Admin Reply:</label>
+                                <textarea id="admin-reply" name="admin-msg" class="w-full border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:border-blue-500" rows="4" placeholder="Enter your reply..."></textarea>
+                            </div>
+                            <div class="flex justify-end">
+                                <button type="submit" name="submit" class="w-full bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Submit Reply</button>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        function openPopupForm(id, x, y, z) {
+            document.getElementById('fid').value = id;
+            document.getElementById('user-name').value = x;
+            document.getElementById('user-feedback').value = y;
+            document.getElementById('user-email').value = z;
+            const popupForm = document.getElementById('popupForm');
+            popupForm.classList.remove('hidden');
+        }
+
+        function closePopupForm() {
+            const popupForm = document.getElementById('popupForm');
+            popupForm.classList.add('hidden');
+        }
+    </script>
+
+
 </body>
 
 </html>
+<?php
+if (isset($_POST['submit'])) {
+    $id = $_POST['fid'];
+
+    $name = $_POST['user-name'];
+    $to = $_POST['user-email'];
+    $msg = $_POST['admin-msg'];
+    $subject = "Feedback Reply";
+    $header = "From:admin@gcs.com";
+    $message = "Dear $name,\n\n";
+    $message .= "Thank you for your feedback.\n\n";
+    $message .= "$msg \n\n";
+    $message .= "Best regards,\n";
+    $message .= "Garbage Complain System";
+
+
+    //qry 
+    $sql = "UPDATE feedback set status = 1 WHERE id=$id";
+    if (mail($to, $subject, $message, $header) && mysqli_query($con, $sql)) {
+        echo '<script>alert("Message Sent.")</script>';
+        echo '<script>window.location.href = "dashboard.php";</script>';
+    } else {
+        echo '<script>alert("Error Sending Message.Please try again later.")</script>';
+    }
+}
+
+?>
