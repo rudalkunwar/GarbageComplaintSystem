da30c2ae24b2c9426c6a80da1e6f2a35bbc42605
ruzal <ruzalkunwar@gmail.com>
2023-09-25 09:51:56 +0545

    db

--- public/database/project_gcs (3).sql
+++ public/database/project_gcs (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 09, 2023 at 11:15 AM
+-- Generation Time: Sep 25, 2023 at 06:05 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -62,10 +62,7 @@ CREATE TABLE `assigned_bin` (
 --
 
 INSERT INTO `assigned_bin` (`assign_id`, `complain_id`, `assigned_driver`, `assignment_date`, `assign_des`, `assign_status`) VALUES
-(33, 30, 'driver1', '2023-08-04', 'your complain is assigned to driver', 'Completed'),
-(36, 35, 'driver3', '2023-08-05', 'collect', 'Rejected'),
-(37, 36, 'driver3', '2023-08-10', 'collect this soon', 'Completed'),
-(40, 40, 'driver3', '2023-08-10', 'yed', 'Completed');
+(43, 43, 'Sohan Kafle', '2023-09-26', 'The driver is assigned for the complained bin.', 'Completed');
 
 -- --------------------------------------------------------
 
@@ -88,10 +85,7 @@ CREATE TABLE `collections` (
 --
 
 INSERT INTO `collections` (`collection_id`, `complain_id`, `assign_id`, `collected_picture`, `collection_des`, `collection_date`, `collection_status`) VALUES
-(29, 30, 33, 'completedtask/images.jpg', 'The bin is collected.', '2023-09-09', 'Completed'),
-(31, 35, 36, NULL, 'rejected', NULL, 'Rejected by Driver'),
-(32, 36, 37, 'completedtask/erdiagram.png', 'sucessfully collected', '2023-08-10', 'Completed'),
-(33, 40, 40, 'completedtask/echofriend2.png', 'collected successfully', '2023-08-10', 'Completed');
+(35, 43, 43, 'completedtask/garbagebin.jpg', 'collected sucessfully', '2023-09-26', 'Completed');
 
 -- --------------------------------------------------------
 
@@ -114,10 +108,7 @@ CREATE TABLE `complaints` (
 --
 
 INSERT INTO `complaints` (`complain_id`, `user_id`, `bin_id`, `description`, `bin_picture`, `timestamp`, `complain_status`) VALUES
-(30, 4, 212, 'This bin is full collect this soon please', 'binpic/6f050e39-windows_10_logoblue.svg-copy_windows.jpg', '2023-08-03 15:50:43', 'Completed'),
-(35, 4, 212, 'c', 'binpic/gausselimination.png', '2023-08-04 03:51:29', 'Rejected by Driver.'),
-(36, 5, 305, 'This bin is full collect this soon.', 'binpic/dfd.png', '2023-08-07 06:14:36', 'Completed'),
-(40, 4, 212, 'this is full', 'binpic/erdiagram.png', '2023-08-08 07:16:01', 'Completed');
+(43, 4, 532, 'this bin is full', 'binpic/garbage bins.jpg', '2023-09-25 03:30:42', 'Completed');
 
 -- --------------------------------------------------------
 
@@ -140,9 +131,8 @@ CREATE TABLE `drivers` (
 --
 
 INSERT INTO `drivers` (`driver_id`, `driver_name`, `password`, `email`, `address`, `driver_picture`, `driver_status`) VALUES
-(400, 'driver3', '3c9e9944fca5fec18378f39af8ead5d8', 'driver3@gmail.com', 'Gaidakot-1', '', 'Free'),
-(1010, 'sohan kafle', '827ccb0eea8a706c4c34a16891f84e7b', 'ankushruzal@gmail.com', 'Gaidakot-1', '', 'Free'),
-(2233, 'driver1', 'b85aef08608180db9d4ddad38ae40545', 'driver1@gmail.com', 'Gaidakot-3', 'profilepic/Screenshot 2023-04-28 224213.png', 'Free');
+(400, 'driver3', '3c9e9944fca5fec18378f39af8ead5d8', 'driver3@gmail.com', 'Gaidakot-1', '', 'Assigned'),
+(1010, 'Sohan Kafle', 'bd68fa6f993d870dbb7971664129685f', 'sohankafle@gmail.com', 'Gaidakot-1', 'profilepic/327833714_1333739860742243_684661946263680325_n.jpg', 'Free');
 
 -- --------------------------------------------------------
 
@@ -164,8 +154,7 @@ CREATE TABLE `feedback` (
 --
 
 INSERT INTO `feedback` (`id`, `name`, `email`, `message`, `timestamp`, `status`) VALUES
-(1, 'admin', 'driver1@gmail.com', 'this website is great and amazing', '2023-06-16 13:05:25', 0),
-(2, 'ruzal', 'aakash@gmail.com', 'this jfaoij jdfkjl dfjkljdlkajdfdfjlfkjlajkldfj djlkajfl kajakjdlakjdfjl afjaf ljkdfjakjfljalkdfj a sajdfjlkjakdfjakldfj asdf lkjsjkdfjljafljalkdjaaklafjd adjdfjlakdfjlakdfjla jfdfjlasjfldajslfj aflsjalfkjal fjdlajdfa klfjasldfj alsjdfjlkajfd akldfj lkasjdffklasj dfljaslkdfj alfjdlkajfdkjadfkdjaslsdf lajafjd klaasjdf;dkasj fkjal fslafj lkfj kasasj dfdkjalfjd la  afjljafd l;adfjl;kajdfj skdfj lsfjd kljas fljasas;fj k aj fdjadfk al;dfjlak jflkaj', '2023-06-16 13:35:14', 1);
+(5, 'Aakash Kandel', 'aakash@gmail.com', 'hey i am aakash', '2023-09-22 11:52:08', 1);
 
 -- --------------------------------------------------------
 
@@ -186,13 +175,10 @@ CREATE TABLE `garbagebins` (
 --
 
 INSERT INTO `garbagebins` (`bin_id`, `type`, `location`, `capacity`, `bin_status`) VALUES
-(212, 'Metallic', 'Bharatpur parbatipur', 100, 'use'),
-(305, 'Organic', 'Bharatpur- Bishnu chow', 100, 'use'),
-(330, 'Inorganic', 'ghorkhali chowk,bharatpur', 100, 'On Collection'),
-(500, 'Organic', 'Kaligandaki chowk', 100, 'use'),
+(330, 'Inorganic', 'ghorkhali chowk,bharatpur', 100, 'use'),
+(500, 'Organic', 'Kaligandaki chowk', 100, 'On Collection'),
 (505, 'Organic', 'Bharatpur-16', 100, 'use'),
-(532, 'Organic', 'Gaidakot-1', 100, 'use'),
-(5920, 'Metallic', 'Bharatpur devi chowk', 120, 'use');
+(532, 'Organic', 'Gaidakot-1', 100, 'use');
 
 -- --------------------------------------------------------
 
@@ -208,6 +194,15 @@ CREATE TABLE `notification` (
   `time` timestamp NOT NULL DEFAULT current_timestamp(),
   `status` int(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `notification`
+--
+
+INSERT INTO `notification` (`id`, `from_id`, `to_id`, `message`, `time`, `status`) VALUES
+(15, 1, 4, 'Your Complain Status is here.', '2023-08-18 10:36:13', 1),
+(16, 1, 400, 'Your Assigned Work is here.', '2023-08-18 10:36:13', 0),
+(21, 1010, 4, 'Your Comlain Report is here.[By Driver]', '2023-09-25 04:01:38', 1);
 
 -- --------------------------------------------------------
 
@@ -230,7 +225,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `user_name`, `email`, `password`, `address`, `contact`, `profilepic`) VALUES
-(4, 'Rudal Kunwar', 'rudalkunwar@gmail.com', '240adebad8e35ddcfe05b24cc062b242', 'Bharatpur-15', '9877665544', ''),
+(4, 'Rudal Kunwar', 'rudalkunwar@gmail.com', '54600cd30d2ba61f88d5b1f105926db5', 'Bharatpur-15', '9877665544', 'profilepic/Screenshot 2023-04-28 224904.png'),
 (5, 'Razz', 'razz@gmail.com', '472ccd35434e09cca8c867e3f25325dc', 'Bharatpur', '9877654212', '');
 
 --
@@ -257,8 +252,9 @@ ALTER TABLE `assigned_bin`
 --
 ALTER TABLE `collections`
   ADD PRIMARY KEY (`collection_id`),
-  ADD UNIQUE KEY `complain_id` (`complain_id`),
-  ADD KEY `assign_id` (`assign_id`);
+  ADD UNIQUE KEY `assign_id_2` (`assign_id`),
+  ADD UNIQUE KEY `assign_id_3` (`assign_id`),
+  ADD KEY `complain_id` (`complain_id`);
 
 --
 -- Indexes for table `complaints`
@@ -313,31 +309,31 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `assigned_bin`
 --
 ALTER TABLE `assigned_bin`
-  MODIFY `assign_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `assign_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `collections`
 --
 ALTER TABLE `collections`
-  MODIFY `collection_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `collection_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `complaints`
 --
 ALTER TABLE `complaints`
-  MODIFY `complain_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `complain_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `notification`
 --
 ALTER TABLE `notification`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -354,21 +350,21 @@ ALTER TABLE `users`
 --
 ALTER TABLE `assigned_bin`
   ADD CONSTRAINT `assigned_bin_ibfk_1` FOREIGN KEY (`assigned_driver`) REFERENCES `drivers` (`driver_name`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `assigned_bin_ibfk_2` FOREIGN KEY (`complain_id`) REFERENCES `complaints` (`complain_id`);
+  ADD CONSTRAINT `assigned_bin_ibfk_2` FOREIGN KEY (`complain_id`) REFERENCES `complaints` (`complain_id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `collections`
 --
 ALTER TABLE `collections`
-  ADD CONSTRAINT `collections_ibfk_1` FOREIGN KEY (`assign_id`) REFERENCES `assigned_bin` (`assign_id`),
-  ADD CONSTRAINT `collections_ibfk_2` FOREIGN KEY (`complain_id`) REFERENCES `complaints` (`complain_id`);
+  ADD CONSTRAINT `collections_ibfk_2` FOREIGN KEY (`assign_id`) REFERENCES `assigned_bin` (`assign_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `collections_ibfk_3` FOREIGN KEY (`complain_id`) REFERENCES `complaints` (`complain_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `complaints`
 --
 ALTER TABLE `complaints`
   ADD CONSTRAINT `complaints_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `complaints_ibfk_2` FOREIGN KEY (`bin_id`) REFERENCES `garbagebins` (`bin_id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `complaints_ibfk_2` FOREIGN KEY (`bin_id`) REFERENCES `garbagebins` (`bin_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
